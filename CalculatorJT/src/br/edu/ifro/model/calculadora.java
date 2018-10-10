6e1e91ca9e444d1c802edefc50a7039133cd5bae
Thimóteo Coelho <thimoteoco@gmail.com>
2018-10-10 16:36:22 -0400

    Menu Principal

--- CalculatorJT/src/br/edu/ifro/model/calculadora.java
+++ CalculatorJT/src/br/edu/ifro/model/calculadora.java
@@ -19,5 +19,44 @@ public class calculadora {
      private Integer id;
      private int primeiroValor, segundoValor, resultado;
      private String operador;
-    
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public int getPrimeiroValor() {
+        return primeiroValor;
+    }
+
+    public void setPrimeiroValor(int primeiroValor) {
+        this.primeiroValor = primeiroValor;
+    }
+
+    public int getSegundoValor() {
+        return segundoValor;
+    }
+
+    public void setSegundoValor(int segundoValor) {
+        this.segundoValor = segundoValor;
+    }
+
+    public int getResultado() {
+        return resultado;
+    }
+
+    public void setResultado(int resultado) {
+        this.resultado = resultado;
+    }
+
+    public String getOperador() {
+        return operador;
+    }
+
+    public void setOperador(String operador) {
+        this.operador = operador;
+    }
 }
